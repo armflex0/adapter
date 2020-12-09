f0197a7a16d040a07536a509be08d56dc99d6aba
armflex0 <59099873+armflex0@users.noreply.github.com>
2020-12-10 03:34:59 +0700

    main

---
+++ wildturkey.cpp
@@ -0,0 +1,13 @@
+#include "wildturkey.h"
+
+WildTurkey::WildTurkey(){
+
+}
+
+void WildTurkey::gobble() {
+    cout << "Gobble" << endl;
+}
+
+void WildTurkey::fly() {
+    cout << "Fly" << endl;
+}
